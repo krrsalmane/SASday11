08c4aabb9afc39f1e6f6b4f9fbc025ddf79e1db8
krrsalmane <karroumsalmane@gmail.com>
2025-09-02 10:59:58 +0100

    finish challenge 2

---
+++ day1/challenge2.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+
+
+int main()
+{
+   double celsius,kelvin;
+    
+    printf("enter the temperator  :");
+    scanf("%f :", &celsius);
+    kelvin=celsius+273.15;
+    printf("temperator in kelvin is\n :",kelvin);
+        return 0;
+}
