9fc13067f5043b78486b01dfe9d60c76f66f8cc2
krrsalmane <karroumsalmane@gmail.com>
2025-09-03 10:33:23 +0100

    function de somme

--- Day 1/main.c
+++ Day 1/main.c
@@ -3,32 +3,21 @@
 
 int main()
 {
-    char name[50];
-    char lastName[50];
-    int age;
-    char email[100];
-    char adresse[100];
 
-    printf("My name is: ");
-    scanf("%49s", name);
 
-    printf("My last name is: ");
-    scanf("%49s", lastName);
+int longueur;
+int largeur;
+int Surface;
+printf("enter the largeur   :");
+scanf("%d",&largeur);
+printf("enter the longueur  :");
+scanf("%d",&longueur);
 
-    printf("I am: ");
-    scanf("%d", &age);
 
-    printf("My email address is: ");
-    scanf("%99s", email);
+Surface = longueur * largeur;
+printf("the surface is : %d",Surface);
 
-    printf("My actual address is: ");
-    scanf("%99s", adresse);
 
-    printf("\n--- User Info ---\n");
-    printf("Name: %s %s\n", name, lastName);
-    printf("Age: %d\n", age);
-    printf("Email: %s\n", email);
-    printf("Address: %s\n", adresse);
 
     return 0;
 }
